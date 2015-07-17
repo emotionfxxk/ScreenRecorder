d282fe3af2cec15e91a2de1ab32d8e0e9e7b7719
Sean.Xu <sean@chinawebeye.com>
2015-07-17 22:42:52 +0800

    get video cursor

--- app/src/main/java/com/mindarc/screenrecorder/utils/StorageHelper.java
+++ app/src/main/java/com/mindarc/screenrecorder/utils/StorageHelper.java
@@ -1,10 +1,17 @@
 package com.mindarc.screenrecorder.utils;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.database.DataSetObserver;
+import android.net.Uri;
 import android.os.Environment;
+import android.os.Handler;
+import android.provider.MediaStore;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -15,30 +22,34 @@ import java.util.Date;
  *
  */
 public class StorageHelper {
-    private final static String MUDULE_TAG = "StorageHelper";
-    private final static String FOLDER_NAME = "ScreenRecorder";
+    private final static String MODULE_TAG = "StorageHelper";
+    private final static String FOLDER_NAME = "AppRecorder";
     private boolean mIsInit = false;
     private boolean mStorageAvailable = false;
     private String mFolderPath;
+    private Cursor mCursor;
+    private Context mAppCtx;
     public final static StorageHelper sStorageHelper = new StorageHelper();
     private StorageHelper() {}
     private BroadcastReceiver mExternalStorageReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            LogUtil.i(MUDULE_TAG, "Storage: " + intent.getData());
+            LogUtil.i(MODULE_TAG, "Storage: " + intent.getData());
             updateStorageState();
         }
     };
     public final synchronized void init(Context ctx) {
         if(!mIsInit) {
             updateStorageState();
-
+            mAppCtx = ctx.getApplicationContext();
             // register BC
             IntentFilter filter = new IntentFilter();
             filter.addAction(Intent.ACTION_MEDIA_MOUNTED);
             filter.addAction(Intent.ACTION_MEDIA_REMOVED);
-            ctx.getApplicationContext().registerReceiver(mExternalStorageReceiver, filter);
+            mAppCtx.registerReceiver(mExternalStorageReceiver, filter);
             mIsInit = true;
+
+            updateVideos();
         }
     }
 
@@ -47,11 +58,16 @@ public class StorageHelper {
     }
 
     public final synchronized void deInit(Context ctx) {
-        LogUtil.i(MUDULE_TAG, "deinit!!");
+        LogUtil.i(MODULE_TAG, "deinit!!");
         mIsInit = false;
         try {
             ctx.unregisterReceiver(mExternalStorageReceiver);
         } catch (Exception e) {
+        }
+        if (mCursor != null) {
+            mCursor.unregisterContentObserver(mContentOb);
+            mCursor.close();
+            mCursor = null;
         }
     }
 
@@ -72,17 +88,61 @@ public class StorageHelper {
             }
         }
         fileName = tempName + ".mp4";
-        LogUtil.i(MUDULE_TAG, "fileName:" + fileName);
+        LogUtil.i(MODULE_TAG, "fileName:" + fileName);
         return fileName;
     }
 
+    private class VideoCollectionContentOb extends ContentObserver {
+        public VideoCollectionContentOb(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            // do s.th.
+            // depending on the handler you might be on the UI
+            // thread, so be cautious!
+            LogUtil.i(MODULE_TAG, "VideoCollectionContentOb onChanged(boolean selfChange, Uri uri)");
+        }
+    }
+
+    private VideoCollectionContentOb mContentOb = new VideoCollectionContentOb(null);
+
+    private void updateVideos() {
+        if (mCursor != null) {
+            mCursor.unregisterContentObserver(mContentOb);
+            mCursor.close();
+            mCursor = null;
+        }
+        ContentResolver cr = mAppCtx.getContentResolver();
+        String selection = MediaStore.Video.Media.DATA + " like?";
+        String[] selectionArgs = new String[]{ "%" + FOLDER_NAME + "%" };
+        String[] parameters = { MediaStore.Video.Media._ID, MediaStore.Video.Media.DISPLAY_NAME};
+        mCursor = cr.query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                parameters, selection, selectionArgs, MediaStore.Video.Media.DATE_TAKEN + " DESC");
+        if (mCursor != null) {
+            LogUtil.i(MODULE_TAG, "mCursor.getCount():" + mCursor.getCount());
+            int columnIndex = mCursor.getColumnIndex(MediaStore.Video.Media._ID);
+            int columnNameIndex = mCursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME);
+            if (mCursor.moveToFirst()) {
+                do {
+                    long id = mCursor.getLong(columnIndex);
+                    String name = mCursor.getString(columnNameIndex);
+                    LogUtil.i(MODULE_TAG, "id:" + id + ", name:" + name);
+                } while (mCursor.moveToNext());
+            }
+            mCursor.registerContentObserver(mContentOb);
+        }
+    }
+
+
     private synchronized void updateStorageState() {
         String state = Environment.getExternalStorageState();
-        LogUtil.i(MUDULE_TAG, "updateStorageState state:" + state);
+        LogUtil.i(MODULE_TAG, "updateStorageState state:" + state);
         if(state.equals(Environment.MEDIA_MOUNTED)) {
             mStorageAvailable = true;
             mFolderPath = System.getenv("EXTERNAL_STORAGE") + File.separator + FOLDER_NAME;
-            LogUtil.i(MUDULE_TAG, "init mFolderPath:" + mFolderPath);
+            LogUtil.i(MODULE_TAG, "init mFolderPath:" + mFolderPath);
             File folder = new File(mFolderPath);
             if(!folder.exists()) folder.mkdirs();
         } else {
